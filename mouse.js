6d72eb6784b7b0e47e11501a4ff7c2186e43d7b3
jonfanderson <32755821+jonfandersonjr@users.noreply.github.com>
2018-01-20 13:26:19 -0800

    Updating local mouse with classes to fill in

---
+++ mouse.js
@@ -0,0 +1,24 @@
+// JavaScript source code
+
+var isBusy = false;
+
+function Mouse() {
+
+}
+
+Mouse.prototype.selectDefender(defenderName) {
+    this.animation = new Animation()
+}
+
+Mouse.prototype.dropTower = function () {
+    //
+    if (isBusy) {
+        //drop tower
+    }
+}
+
+
+
+//Send in mouse class to game engine
+//Modify stuff in game engine
+//this.mouse.
