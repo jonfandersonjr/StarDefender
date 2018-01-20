d010e373793fb915ed5ea85afc62fc8aae47392d
Jacob Reed <jacob.russell.reed@gmail.com>
2018-01-19 16:03:49 -0800

    Added UI Image on bottom canvas

--- main.js
+++ main.js
@@ -85,19 +85,27 @@ for (let i = 0; i < unitList.length; i++) {
 AM.queueDownload("./tiles/dirt.png");
 AM.queueDownload("./tiles/grass.png");
 
-
 AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
     var ctx = canvas.getContext("2d");
-
     var gameEngine = new GameEngine();
     gameEngine.init(ctx);
     gameEngine.start();
 
     var map = new Map(map_1);
     createMap(gameEngine, map);
+    console.log("Map Loaded!");
+
     gameEngine.addEntity(new GroundUnit(gameEngine, "martarlisk", map.dIni, map, AM));
     gameEngine.addEntity(new GroundUnit(gameEngine, "stroach", map.dIni, map, AM));
-    gameEngine.addEntity(new GroundUnit(gameEngine, "sergling", map.dIni, map, AM));
-    console.log("All Done!");
+    console.log("Enemies Loaded!");
+
+    //UI Load
+    ctx = document.getElementById("ui").getContext("2d");
+    var imageObj = new Image();
+    imageObj.src = './ui/ui.png';
+    imageObj.onload = function() {
+      ctx.drawImage(imageObj, 0, 0);
+    };
+    console.log("UI Loaded!");
 });
--- ui.js
+++ ui.js
@@ -4,3 +4,4 @@ Create Resource Bar
 Create Tower Icons
 
 */
+ctx = document.getElementById("ui").getContext("2d");
