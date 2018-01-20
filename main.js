5f333b443799318be811aaa376a217dcc93610cc
Tuan Dinh <qdinh@uw.edu>
2018-01-20 00:52:04 -0800

    Moved animation to its own file -Tuan

--- main.js
+++ main.js
@@ -1,50 +1,5 @@
 var AM = new AssetManager();
 
-function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
-	this.spriteSheet = spriteSheet;
-	this.frameWidth = frameWidth;
-	this.frameDuration = frameDuration;
-	this.frameHeight = frameHeight;
-	this.sheetWidth = sheetWidth;
-	this.frames = frames;
-	this.totalTime = frameDuration * frames;
-	this.elapsedTime = 0;
-	this.loop = loop;
-	this.scale = scale;
-}
-
-Animation.prototype.setSprite = function (spriteSheet) {
-	this.spriteSheet = spriteSheet;
-}
-
-Animation.prototype.drawFrame = function (tick, ctx, x, y) {
-	this.elapsedTime += tick;
-	if (this.isDone()) {
-		if (this.loop)
-			this.elapsedTime = 0;
-	}
-	var frame = this.currentFrame();
-	var xindex = 0;
-	var yindex = 0;
-	xindex = frame % this.sheetWidth;
-	yindex = Math.floor(frame / this.sheetWidth);
-
-	ctx.drawImage(this.spriteSheet,
-		xindex * this.frameWidth, yindex * this.frameHeight, // source from sheet
-		this.frameWidth, this.frameHeight,
-		x, y,
-		this.frameWidth * this.scale,
-		this.frameHeight * this.scale);
-}
-
-Animation.prototype.currentFrame = function () {
-	return Math.floor(this.elapsedTime / this.frameDuration);
-}
-
-Animation.prototype.isDone = function () {
-	return (this.elapsedTime >= this.totalTime);
-}
-
 function Background(game, spritesheet, x, y) {
 	this.x = x;
 	this.y = y;
