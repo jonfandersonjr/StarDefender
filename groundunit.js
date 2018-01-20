4786b3cf791eae7757be97734732b78f56ba63bc
Tuan Dinh <qdinh@uw.edu>
2018-01-20 00:26:29 -0800

    Only changed formatting -Tuan

--- groundunit.js
+++ groundunit.js
@@ -5,32 +5,32 @@ var stroach = [75, 68, 5, 0.1, 5, true, 0.5, 25];
 var sergling = [45, 45, 7, 0.1, 7, true, .5, 75];
 
 function GroundUnit(game, unitName, direction, map, assetManager) {
-    this.AM = assetManager;
-    this.unitName = unitName;
+	this.AM = assetManager;
+	this.unitName = unitName;
 
-    //Switch case for units.
-    switch(unitName) {
-        case "martarlisk":
-            this.settings = martarlisk;
-            break;
-        case "stroach":
-            this.settings = stroach;
-            break;
-        case "sergling":
-            this.settings = sergling;
-            break;
-        default:
-    }
-    this.animation = new Animation(this.AM.getAsset(`./img/${this.unitName}_${direction}.png`),
-                                    this.settings[0], this.settings[1], this.settings[2], this.settings[3], this.settings[4], this.settings[5], this.settings[6]);
-    this.speed = this.settings[7];
-    this.ctx = game.ctx;
-    this.direction = direction;
-    this.map = map;
-    this.x = this.map.xIni * this.map.tileSize;
-    this.y = this.map.yIni * this.map.tileSize;
+	//Switch case for units.
+	switch (unitName) {
+	case "martarlisk":
+		this.settings = martarlisk;
+		break;
+	case "stroach":
+		this.settings = stroach;
+		break;
+	case "sergling":
+		this.settings = sergling;
+		break;
+	default:
+	}
+	this.animation = new Animation(this.AM.getAsset(`./img/${this.unitName}_${direction}.png`),
+			this.settings[0], this.settings[1], this.settings[2], this.settings[3], this.settings[4], this.settings[5], this.settings[6]);
+	this.speed = this.settings[7];
+	this.ctx = game.ctx;
+	this.direction = direction;
+	this.map = map;
+	this.x = this.map.xIni * this.map.tileSize;
+	this.y = this.map.yIni * this.map.tileSize;
 
-    Entity.call(this, game, this.x , this.y );
+	Entity.call(this, game, this.x, this.y);
 }
 
 GroundUnit.prototype = new Entity();
@@ -38,63 +38,63 @@ GroundUnit.prototype.constructor = GroundUnit;
 
 //Calculates new coordinate based on current direction. If the next tile is not path, call changeDirection to find new direction.
 GroundUnit.prototype.update = function () {
-    let canvas = document.getElementById("gameWorld");
-    let row = Math.floor(this.x / this.map.tileSize);
-    let column = Math.floor(this.y / this.map.tileSize)
-    if(this.direction === "east") {
-        this.x += this.game.clockTick * this.speed; //Next position
-        if (this.map.map[Math.floor(this.x / this.map.tileSize) + 1 + column * this.map.mapSize] === '+') { //Checks if next position is a path.
-            row = Math.floor(this.x / this.map.tileSize);
-            this.changeDirection(newDirection(this.map, row, column, this.direction));
-        }
-    } else if (this.direction === "west") {
-        let tempX = this.x - this.game.clockTick * this.speed; //Next position
-        if (this.map.map[Math.floor(tempX / this.map.tileSize) + column * this.map.mapSize] === '+') { //Checks if next position is a path.
-            this.changeDirection(newDirection(this.map, row, column, this.direction));
-        } else {
-            this.x = tempX;
-        }
-    } else if (this.direction === "south") {
-        this.y += this.game.clockTick * this.speed; //Next position
-        if (this.map.map[row + (Math.floor(this.y / this.map.tileSize) + 1) * this.map.mapSize] === '+') { //Checks if next position is a path.
-            column = Math.floor(this.y / this.map.tileSize);
-            this.changeDirection(newDirection(this.map, row, column, this.direction));
-        }
-    } else if (this.direction === "north") {
-        let tempY = this.y - this.game.clockTick * this.speed; //Next position
-        if (this.map.map[row + Math.floor(tempY / this.map.tileSize) * this.map.mapSize] === '+') { //Checks if next position is a path.
-            this.changeDirection(newDirection(this.map, row, column, this.direction));
-        } else {
-            this.y = tempY;
-        }
-    }
-    Entity.prototype.update.call(this);
+	let canvas = document.getElementById("gameWorld");
+	let row = Math.floor(this.x / this.map.tileSize);
+	let column = Math.floor(this.y / this.map.tileSize)
+		if (this.direction === "east") {
+			this.x += this.game.clockTick * this.speed; //Next position
+			if (this.map.map[Math.floor(this.x / this.map.tileSize) + 1 + column * this.map.mapSize] === '+') { //Checks if next position is a path.
+				row = Math.floor(this.x / this.map.tileSize);
+				this.changeDirection(newDirection(this.map, row, column, this.direction));
+			}
+		} else if (this.direction === "west") {
+			let tempX = this.x - this.game.clockTick * this.speed; //Next position
+			if (this.map.map[Math.floor(tempX / this.map.tileSize) + column * this.map.mapSize] === '+') { //Checks if next position is a path.
+				this.changeDirection(newDirection(this.map, row, column, this.direction));
+			} else {
+				this.x = tempX;
+			}
+		} else if (this.direction === "south") {
+			this.y += this.game.clockTick * this.speed; //Next position
+			if (this.map.map[row + (Math.floor(this.y / this.map.tileSize) + 1) * this.map.mapSize] === '+') { //Checks if next position is a path.
+				column = Math.floor(this.y / this.map.tileSize);
+				this.changeDirection(newDirection(this.map, row, column, this.direction));
+			}
+		} else if (this.direction === "north") {
+			let tempY = this.y - this.game.clockTick * this.speed; //Next position
+			if (this.map.map[row + Math.floor(tempY / this.map.tileSize) * this.map.mapSize] === '+') { //Checks if next position is a path.
+				this.changeDirection(newDirection(this.map, row, column, this.direction));
+			} else {
+				this.y = tempY;
+			}
+		}
+		Entity.prototype.update.call(this);
 }
 
 GroundUnit.prototype.draw = function () {
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
-    Entity.prototype.draw.call(this);
+	this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+	Entity.prototype.draw.call(this);
 }
 
 GroundUnit.prototype.changeDirection = function (direction) {
-    this.direction = direction;
-    this.animation.setSprite(this.AM.getAsset(`./img/${this.unitName}_${direction}.png`));
+	this.direction = direction;
+	this.animation.setSprite(this.AM.getAsset(`./img/${this.unitName}_${direction}.png`));
 }
 
 //Finds new direction by checking tiles next to the current one (x, y). Should not go back to where it came from.
 function newDirection(map, x, y, currentDirection) {
-    if (currentDirection === "east" || currentDirection === "west") {
+	if (currentDirection === "east" || currentDirection === "west") {
 
-        if (map.map[x + (y - 1) * map.mapSize] === '-') {
-            return "north";
-        } else {
-            return "south";
-        }
-    } else {
-        if (map.map[x - 1 + y * map.mapSize] === '-') {
-            return "west";
-        } else {
-            return "east";
-        }
-    }
+		if (map.map[x + (y - 1) * map.mapSize] === '-') {
+			return "north";
+		} else {
+			return "south";
+		}
+	} else {
+		if (map.map[x - 1 + y * map.mapSize] === '-') {
+			return "west";
+		} else {
+			return "east";
+		}
+	}
 }
--- main.js
+++ main.js
@@ -1,112 +1,112 @@
 var AM = new AssetManager();
 
 function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
-    this.spriteSheet = spriteSheet;
-    this.frameWidth = frameWidth;
-    this.frameDuration = frameDuration;
-    this.frameHeight = frameHeight;
-    this.sheetWidth = sheetWidth;
-    this.frames = frames;
-    this.totalTime = frameDuration * frames;
-    this.elapsedTime = 0;
-    this.loop = loop;
-    this.scale = scale;
+	this.spriteSheet = spriteSheet;
+	this.frameWidth = frameWidth;
+	this.frameDuration = frameDuration;
+	this.frameHeight = frameHeight;
+	this.sheetWidth = sheetWidth;
+	this.frames = frames;
+	this.totalTime = frameDuration * frames;
+	this.elapsedTime = 0;
+	this.loop = loop;
+	this.scale = scale;
 }
 
 Animation.prototype.setSprite = function (spriteSheet) {
-    this.spriteSheet = spriteSheet;
+	this.spriteSheet = spriteSheet;
 }
 
 Animation.prototype.drawFrame = function (tick, ctx, x, y) {
-    this.elapsedTime += tick;
-    if (this.isDone()) {
-        if (this.loop) this.elapsedTime = 0;
-    }
-    var frame = this.currentFrame();
-    var xindex = 0;
-    var yindex = 0;
-    xindex = frame % this.sheetWidth;
-    yindex = Math.floor(frame / this.sheetWidth);
+	this.elapsedTime += tick;
+	if (this.isDone()) {
+		if (this.loop)
+			this.elapsedTime = 0;
+	}
+	var frame = this.currentFrame();
+	var xindex = 0;
+	var yindex = 0;
+	xindex = frame % this.sheetWidth;
+	yindex = Math.floor(frame / this.sheetWidth);
 
-    ctx.drawImage(this.spriteSheet,
-                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
-                 this.frameWidth, this.frameHeight,
-                 x, y,
-                 this.frameWidth * this.scale,
-                 this.frameHeight * this.scale);
+	ctx.drawImage(this.spriteSheet,
+		xindex * this.frameWidth, yindex * this.frameHeight, // source from sheet
+		this.frameWidth, this.frameHeight,
+		x, y,
+		this.frameWidth * this.scale,
+		this.frameHeight * this.scale);
 }
 
 Animation.prototype.currentFrame = function () {
-    return Math.floor(this.elapsedTime / this.frameDuration);
+	return Math.floor(this.elapsedTime / this.frameDuration);
 }
 
 Animation.prototype.isDone = function () {
-    return (this.elapsedTime >= this.totalTime);
+	return (this.elapsedTime >= this.totalTime);
 }
 
 function Background(game, spritesheet, x, y) {
-    this.x = x;
-    this.y = y;
-    this.spritesheet = spritesheet;
-    this.game = game;
-    this.ctx = game.ctx;
+	this.x = x;
+	this.y = y;
+	this.spritesheet = spritesheet;
+	this.game = game;
+	this.ctx = game.ctx;
 }
 
 Background.prototype.draw = function () {
-    this.ctx.drawImage(this.spritesheet,
-                   this.x, this.y);
+	this.ctx.drawImage(this.spritesheet,
+		this.x, this.y);
 };
 
-Background.prototype.update = function () {
-};
+Background.prototype.update = function () {};
 
 //Creates a map based on the selection.
 function createMap(game, map) {
-    var mapString = map.map;
-    for (let i = 0, j = 0; i < map.mapSize; i++) {
-        if (mapString[i + j * map.mapSize] === '+') {
-            game.addEntity(new Background(game, AM.getAsset("./tiles/grass.png"), i * map.tileSize, j * map.tileSize));
-        } else if (mapString[i + j * map.mapSize] === '\n') {
-            i = -1;
-            j++;
-        } else if (mapString[i + j * map.mapSize] === '-') {
-            game.addEntity(new Background(game, AM.getAsset("./tiles/dirt.png"), i * map.tileSize, j * map.tileSize));
-        }
-    }
+	var mapString = map.map;
+	for (let i = 0, j = 0; i < map.mapSize; i++) {
+		if (mapString[i + j * map.mapSize] === '+') {
+			game.addEntity(new Background(game, AM.getAsset("./tiles/grass.png"), i * map.tileSize, j * map.tileSize));
+		} else if (mapString[i + j * map.mapSize] === '\n') {
+			i = -1;
+			j++;
+		} else if (mapString[i + j * map.mapSize] === '-') {
+			game.addEntity(new Background(game, AM.getAsset("./tiles/dirt.png"), i * map.tileSize, j * map.tileSize));
+		}
+	}
 }
 
 var unitList = ["martarlisk", "stroach", "sergling"];
 var directions = ["east", "west", "north", "south"];
 for (let i = 0; i < unitList.length; i++) {
-    for (let j = 0; j < directions.length; j++) {
-        AM.queueDownload(`./img/${unitList[i]}_${directions[j]}.png`);
-    }
+	for (let j = 0; j < directions.length; j++) {
+		AM.queueDownload(`./img/${unitList[i]}_${directions[j]}.png`);
+	}
 }
 AM.queueDownload("./tiles/dirt.png");
 AM.queueDownload("./tiles/grass.png");
 
 AM.downloadAll(function () {
-    var canvas = document.getElementById("gameWorld");
-    var ctx = canvas.getContext("2d");
-    var gameEngine = new GameEngine();
-    gameEngine.init(ctx);
-    gameEngine.start();
+	var canvas = document.getElementById("gameWorld");
+	var ctx = canvas.getContext("2d");
+	var gameEngine = new GameEngine();
+	gameEngine.init(ctx);
+	gameEngine.start();
 
-    var map = new Map(map_1);
-    createMap(gameEngine, map);
-    console.log("Map Loaded!");
+	var map = new Map(map_1);
+	createMap(gameEngine, map);
+	console.log("Map Loaded!");
 
-    gameEngine.addEntity(new GroundUnit(gameEngine, "martarlisk", map.dIni, map, AM));
-    gameEngine.addEntity(new GroundUnit(gameEngine, "stroach", map.dIni, map, AM));
-    gameEngine.addEntity(new GroundUnit(gameEngine, "sergling", map.dIni, map, AM));
-    console.log("Enemies Loaded!");
+	gameEngine.addEntity(new GroundUnit(gameEngine, "martarlisk", map.dIni, map, AM));
+	gameEngine.addEntity(new GroundUnit(gameEngine, "stroach", map.dIni, map, AM));
+	gameEngine.addEntity(new GroundUnit(gameEngine, "sergling", map.dIni, map, AM));
+	console.log("Enemies Loaded!");
 
-    //UI Load
-    ctx = document.getElementById("ui").getContext("2d");
-    var imageObj = new Image();
-    imageObj.src = './ui/ui.png';
-    imageObj.onload = function() {
-      ctx.drawImage(imageObj, 0, 0);
-    };
-    console.log("UI Loaded!");
+	//UI Load
+	ctx = document.getElementById("ui").getContext("2d");
+	var imageObj = new Image();
+	imageObj.src = './ui/ui.png';
+	imageObj.onload = function () {
+		ctx.drawImage(imageObj, 0, 0);
+	};
+	console.log("UI Loaded!");
 });
