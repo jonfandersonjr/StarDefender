1f0782d4a8f03016fdb4e5d5f9fb474ad412ccae
jonfanderson <32755821+jonfandersonjr@users.noreply.github.com>
2018-01-21 12:01:46 -0800

    Added comments

--- generator.js
+++ generator.js
@@ -1,6 +1,3 @@
-// JavaScript source code
-
-// JavaScript source code
 
 function Generator(engine, map, assetManager) {
     this.doc = document.getElementById("gameWorld");
@@ -9,10 +6,12 @@ function Generator(engine, map, assetManager) {
     this.AM = assetManager;
 }
 
+//Creates enemy of specified type at beginning of given map
 Generator.prototype.createEnemy = function (enemyName) {
     this.gameEngine.addEntity(new GroundUnit(this.gameEngine, enemyName, this.map.dIni, this.map, this.AM));
 }
 
+//Creates defender of given type at a location specified by the mouse
 Generator.prototype.createDefender = function (defenderName, x, y) {
     this.gameEngine.addEntity(new Defender(this.gameEngine, defenderName, x, y, this.map, this.AM));
 }
