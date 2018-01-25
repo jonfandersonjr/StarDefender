a53f37d318c10be0a0d18611d01020180c17a976
Tuan Dinh <qdinh@uw.edu>
2018-01-24 18:48:41 -0800

    Fixed Serling's animation

--- groundunit.js
+++ groundunit.js
@@ -2,7 +2,7 @@
 //frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale, speed
 var martarlisk = [64, 72, 5, 0.1, 5, true, 0.5, 50];
 var stroach = [75, 68, 5, 0.1, 5, true, 0.5, 25];
-var sergling = [45, 45, 7, 0.1, 7, true, 0.6, 75];
+var sergling = [40, 39, 7, 0.1, 7, true, 0.6, 75];
 
 function GroundUnit(game, unitName, direction, map, assetManager, speedSetting) {
 	this.AM = assetManager;
