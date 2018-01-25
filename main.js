7f730aeb42a5b0955cb106bb6610b6efce583281
Jacob Reed <jacob.russell.reed@gmail.com>
2018-01-24 20:21:18 -0800

    Beautify
    Beautified code for Tuan, so he doesnt get triggered

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ for (let i = 0; i < defenderList.length; i++) {
 //load enemy sprites
 var unitList = ["martarlisk", "stroach", "sergling"];
 for (let i = 0; i < unitList.length; i++) {
-	for (let j = 0; j < directions.length; j++) {
+    for (let j = 0; j < directions.length; j++) {
         AM.queueDownload(`./img/${unitList[i]}/${unitList[i]}_${directions[j]}.png`);
-	}
+    }
 }
 
 //load tiles
@@ -22,39 +22,39 @@ AM.queueDownload("./tiles/dirt.png");
 AM.queueDownload("./tiles/grass.png");
 AM.queueDownload("./tiles/base.png")
 
-AM.downloadAll(function () {
+AM.downloadAll(function() {
     var canvas = document.getElementById("gameWorld");
     var ctx = canvas.getContext("2d");
     var map = new Map(map_1);
     var myMouse = new Mouse(map, ctx)
 
-	//UI Load
-  canvas.style.outlineColor = "#000000"; //prevent highlighting
-	buttonCanvas = document.getElementById("uiButtons");
-	textCanvas = document.getElementById("uiText");
+    //UI Load
+    canvas.style.outlineColor = "#000000"; //prevent highlighting
+    buttonCanvas = document.getElementById("uiButtons");
+    textCanvas = document.getElementById("uiText");
 
-	this.ui = new UI(buttonCanvas, textCanvas, myMouse, 100, 100, 100, 1, 0, 0);
+    this.ui = new UI(buttonCanvas, textCanvas, myMouse, 100, 100, 100, 1, 0, 0);
     console.log("UI Loaded!");
 
-	var gameEngine = new GameEngine(myMouse, this.ui);
+    var gameEngine = new GameEngine(myMouse, this.ui);
 
 
     //This generator will allow us to easily create enemies or towers and not just in main when the code first loads
     this.generator = new Generator(gameEngine, map, AM);
     myMouse.setGenerator(this.generator);
 
-	//Game Engine Start
-	gameEngine.init(ctx);
-	gameEngine.start();
+    //Game Engine Start
+    gameEngine.init(ctx);
+    gameEngine.start();
 
-	//Map Load
-	map.createMap(gameEngine, AM);
+    //Map Load
+    map.createMap(gameEngine, AM);
     console.log("Map Loaded!");
 
-	//Load in entities for prototype
+    //Load in entities for prototype
     this.generator.createEnemy("martarlisk");
     this.generator.createEnemy("stroach");
     this.generator.createEnemy("sergling");
-	console.log("Enemies Loaded!");
+    console.log("Enemies Loaded!");
 
 });
--- ui.js
+++ ui.js
@@ -5,14 +5,16 @@ textCanvas - textArea that holds this
 */
 function UI(buttonCanvas, textCanvas, mouse, startHealth, maxHealth,
     startRes, startLevel, wavesCleared, enemiesK) {
-        this.canvas = buttonCanvas;
-        this.ctx = this.canvas.getContext("2d");
-        this.textBox = textCanvas;
-        makeUnselectable(this.textBox);
-        this.mouse = mouse;
-        this.textBox.addEventListener('mousedown', function(e){ e.preventDefault(); }, false);
+    this.canvas = buttonCanvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.textBox = textCanvas;
+    makeUnselectable(this.textBox);
+    this.mouse = mouse;
+    this.textBox.addEventListener('mousedown', function(e) {
+        e.preventDefault();
+    }, false);
 
-  /*//Load UI Image on image canvas
+    /*//Load UI Image on image canvas
   var imageObj = new Image();
   imageObj.src = './img/ui/defenseUIButtons.png';
 	imageObj.onload = function () {
@@ -20,164 +22,186 @@ function UI(buttonCanvas, textCanvas, mouse, startHealth, maxHealth,
 	};
   console.log("UI Image Loaded!");*/
 
-  //Button generator array
-  const buttons = [
-    {
-      x: 0, y: 150, width: 100, height: 100, color: 'rgb(255,0,0)', id:'marine'
-    },
-    {
-      x: 140, y: 150, width: 100, height: 100, color: 'rgb(255,150,0)', id:'2'
-    },
-    {
-      x: 0, y: 270, width: 100, height: 100, color: 'rgb(255,200,0)', id:'3'
-    },
-    {
-      x: 140, y: 270, width: 100, height: 100, color: 'rgb(255,250,0)', id:'4'
-    }];
+    //Button generator array
+    const buttons = [{
+            x: 0,
+            y: 150,
+            width: 100,
+            height: 100,
+            color: 'rgb(255,0,0)',
+            id: 'marine'
+        },
+        {
+            x: 140,
+            y: 150,
+            width: 100,
+            height: 100,
+            color: 'rgb(255,150,0)',
+            id: '2'
+        },
+        {
+            x: 0,
+            y: 270,
+            width: 100,
+            height: 100,
+            color: 'rgb(255,200,0)',
+            id: '3'
+        },
+        {
+            x: 140,
+            y: 270,
+            width: 100,
+            height: 100,
+            color: 'rgb(255,250,0)',
+            id: '4'
+        }
+    ];
 
     //Draw buttons
     buttons.forEach(button => {
-      this.ctx.beginPath();
-      this.ctx.rect(button.x, button.y, button.width, button.height);
-      this.ctx.fillStyle = button.color;
-      this.ctx.fill();
+        this.ctx.beginPath();
+        this.ctx.rect(button.x, button.y, button.width, button.height);
+        this.ctx.fillStyle = button.color;
+        this.ctx.fill();
     });
 
     //Returns true if mouse pointer is on button
     function isIntersect(mp, button) {
-      if(mp.x >= button.x && mp.x <= button.x + button.width &&
-          mp.y >= button.y && mp.y <= button.y + button.height) {
+        if (mp.x >= button.x && mp.x <= button.x + button.width &&
+            mp.y >= button.y && mp.y <= button.y + button.height) {
             return true;
-          } else {
+        } else {
             return false;
-          }
+        }
     };
 
     //gets relative mouse position based on canvas
     function getMousePos(canvas, e) {
-      var rect = canvas.getBoundingClientRect();
-      return {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top
-      };
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: e.clientX - rect.left,
+            y: e.clientY - rect.top
+        };
     }
 
     //Add mouse click listener which gets mouse point and compares to each button area
     this.canvas.addEventListener('click', (e) => {
-      var mousePos = getMousePos(this.canvas, e);
-      buttons.forEach(theButton => {
-        if(isIntersect(mousePos, theButton)) {
-          mouse.selectDefender(theButton.id);
-        }
-      });
+        var mousePos = getMousePos(this.canvas, e);
+        buttons.forEach(theButton => {
+            if (isIntersect(mousePos, theButton)) {
+                mouse.selectDefender(theButton.id);
+            }
+        });
     });
 
 
-  //init values
-  this.inithealthCur = startHealth;
-  this.inithealthMax = maxHealth;
-  this.initresourcesTotal = startRes;
-  this.initcurLevel = startLevel;
-  this.initwavesC = wavesCleared;
-  this.initenemiesKilled = enemiesK;
+    //init values
+    this.inithealthCur = startHealth;
+    this.inithealthMax = maxHealth;
+    this.initresourcesTotal = startRes;
+    this.initcurLevel = startLevel;
+    this.initwavesC = wavesCleared;
+    this.initenemiesKilled = enemiesK;
 
-  this.healthCur = startHealth;
-  this.healthMax = maxHealth;
-  this.resourcesTotal = startRes;
-  this.curLevel = startLevel;
-  this.wavesC = wavesCleared;
-  this.enemiesKilled = enemiesK;
+    this.healthCur = startHealth;
+    this.healthMax = maxHealth;
+    this.resourcesTotal = startRes;
+    this.curLevel = startLevel;
+    this.wavesC = wavesCleared;
+    this.enemiesKilled = enemiesK;
 
-  this.time = "00:00";
+    this.time = "00:00";
 
-  //Load Default Text
-  this.updateText();
-  console.log("Default stats text loaded!");
+    //Load Default Text
+    this.updateText();
+    console.log("Default stats text loaded!");
 };
 
-UI.prototype.updateText = function () {
-          var tempString = "Health: ";
-          tempString += this.healthCur + " / " + this.healthMax + "\n";
-          tempString += "Resources: " + this.resourcesTotal + "\n";
-          tempString += "Level: " + this.curLevel + "\n";
-          tempString += "Waves Cleared: " + this.wavesC + "\n";
-          tempString += "Enemies Killed: " + this.enemiesKilled + "\n";
-          tempString += "Time: " + this.time + "\n";
-          this.textBox.value = tempString;
+UI.prototype.updateText = function() {
+    var tempString = "Health: ";
+    tempString += this.healthCur + " / " + this.healthMax + "\n";
+    tempString += "Resources: " + this.resourcesTotal + "\n";
+    tempString += "Level: " + this.curLevel + "\n";
+    tempString += "Waves Cleared: " + this.wavesC + "\n";
+    tempString += "Enemies Killed: " + this.enemiesKilled + "\n";
+    tempString += "Time: " + this.time + "\n";
+    this.textBox.value = tempString;
 }
 
 //Takes health away from current health pool
-UI.prototype.dmg = function (amount) {
-  this.healthCur -= amount;
-  this.updateText();
+UI.prototype.dmg = function(amount) {
+    this.healthCur -= amount;
+    this.updateText();
 }
 
 //Adjust resource + or -
-UI.prototype.resourceAdjust = function (amount) {
-  this.resourcesTotal += amount;
-  this.updateText();
+UI.prototype.resourceAdjust = function(amount) {
+    this.resourcesTotal += amount;
+    this.updateText();
 }
 
 //Adjust waves Cleared
 UI.prototype.wavesAdjust = function(amount) {
-  this.wavesC += amount;
-  this.updateText();
+    this.wavesC += amount;
+    this.updateText();
 }
 
 //Adjust enemies Killed
 UI.prototype.enemiesKilledAdjust = function(amount) {
-  this.enemiesKilled += amount;
-  this.updateText();
+    this.enemiesKilled += amount;
+    this.updateText();
 }
 
 //Adjust level
 UI.prototype.adjustLevel = function(amount) {
-  this.curLevel += amount;
-  this.updateText();
+    this.curLevel += amount;
+    this.updateText();
 }
 
 //Reset this to init values from initial new Stat() call
 UI.prototype.reset = function() {
-  this.healthCur = this.inithealthCur;
-  this.healthMax = this.inithealthMax;
-  this.resourcesTotal = this.initresourcesTotal;
-  this.curLevel = this.initcurLevel;
-  this.wavesC = this.initwavesC;
-  this.enemiesKilled = this.initenemiesKilled;
-  this.time = this.inittime;
-  this.updateText();
+    this.healthCur = this.inithealthCur;
+    this.healthMax = this.inithealthMax;
+    this.resourcesTotal = this.initresourcesTotal;
+    this.curLevel = this.initcurLevel;
+    this.wavesC = this.initwavesC;
+    this.enemiesKilled = this.initenemiesKilled;
+    this.time = this.inittime;
+    this.updateText();
 }
 
-UI.prototype.updateTime = function (value) {
+UI.prototype.updateTime = function(value) {
     var timeString = parseFloat(value).toFixed(2);
     var timeString = Math.floor(timeString);
 
     if (timeString < 10) { //seconds format < 10
         timeString = "00:0" + timeString;
         this.time = timeString;
-    } else if(timeString < 60) { //seconds format
-          timeString = "00:" + timeString;
-          this.time = timeString;
+    } else if (timeString < 60) { //seconds format
+        timeString = "00:" + timeString;
+        this.time = timeString;
     } else { //Minutes:Seconds format
-          minutes = Math.floor(timeString / 60);
-          seconds = Math.floor(timeString % 60);
-          if (seconds < 10) {
+        minutes = Math.floor(timeString / 60);
+        seconds = Math.floor(timeString % 60);
+        if (seconds < 10) {
             timeString = minutes + ":0" + seconds;
-          } else {
+        } else {
             timeString = minutes + ":" + seconds;
-          }
-          this.time = timeString;
+        }
+        this.time = timeString;
     }
     this.updateText();
 }
 
 function makeUnselectable(elem) {
-  if (typeof(elem) == 'string')
-    elem = document.getElementById(elem);
-  if (elem) {
-    elem.onselectstart = function() { return false; };
-    elem.style.MozUserSelect = "none";
-    elem.style.KhtmlUserSelect = "none";
-    elem.unselectable = "on";
-  }
+    if (typeof(elem) == 'string')
+        elem = document.getElementById(elem);
+    if (elem) {
+        elem.onselectstart = function() {
+            return false;
+        };
+        elem.style.MozUserSelect = "none";
+        elem.style.KhtmlUserSelect = "none";
+        elem.unselectable = "on";
+    }
 }
