1aaec7e924b2d4d4fc7a61c44435c37e4cae7c3a
jonfanderson <32755821+jonfandersonjr@users.noreply.github.com>
2018-02-03 16:33:48 -0800

    refactored level

--- level.js
+++ level.js
@@ -11,6 +11,7 @@ var secondLevelWaveSize = [5, 3, 7, 4, 10, 10, 8];
 var secondLevelSpeedBuff = [1, 1, 2, 1.5, 1, 1.75];
 var secondLevelHealthBuff = [1, 1.5, 2, 3, 4, .5];
 
+
 function Level(levelNum, waveObject) {
     this.canvas = document.getElementById("gameWorld");
     this.levelNum = levelNum;
@@ -27,22 +28,25 @@ Level.prototype.createWave = function () {
 
     console.log("Sending wave " + this.waveNumber + " on level " + this.levelNum);
 
-    if (this.levelNum === 1) {
-
-        this.wave.setWave(firstLevelEnemies[this.waveNumber],
-            firstLevelWaveSize[this.waveNumber],
-            firstLevelSpeedBuff[this.waveNumber],
-            firstLevelHealthBuff[this.waveNumber]);
-
-        this.waveNumber++;
-    } else if (this.levelNum === 2) {
-
-        this.wave.setWave(secondLevelEnemies[this.waveNumber],
-            secondLevelWaveSize[this.waveNumber],
-            secondLevelSpeedBuff[this.waveNumber],
-            secondLevelHealthBuff[this.waveNumber]);
-
-        this.waveNumber++;
+    switch (this.levelNum) {
+        case 1:
+            this.playLevel1();
+            break;
+        case 2:
+            this.playLevel2();
+            break;
+        case 3:
+            this.playLevel3();
+            break;
+        case 4:
+            this.playLevel4();
+            break;
+        case 5:
+            this.playLevel5();
+            break;
+        default:
+            console.log("illegal level selection of level: " + this.levelNum);
+            break;
     }
 
     //Once all the waves have run, end the level.
@@ -55,3 +59,34 @@ Level.prototype.createWave = function () {
 
 };
 
+Level.prototype.playLevel1 = function () {
+    this.wave.setWave(firstLevelEnemies[this.waveNumber],
+                      firstLevelWaveSize[this.waveNumber],
+                      firstLevelSpeedBuff[this.waveNumber],
+                      firstLevelHealthBuff[this.waveNumber]);
+    this.waveNumber++;
+}
+
+Level.prototype.playLevel2 = function () {
+    this.wave.setWave(secondLevelEnemies[this.waveNumber],
+                      secondLevelWaveSize[this.waveNumber],
+                      secondLevelSpeedBuff[this.waveNumber],
+                      secondLevelHealthBuff[this.waveNumber]);
+    this.waveNumber++;
+}
+
+Level.prototype.playLevel3 = function () {
+    this.wave.setWave(thirdLevelEnemies[this.waveNumber],
+                      thirdLevelWaveSize[this.waveNumber],
+                      thirdLevelSpeedBuff[this.waveNumber],
+                      thirdLevelHealthBuff[this.waveNumber]);
+    this.waveNumber++;
+}
+
+Level.prototype.playLevel4 = function () {
+
+}
+
+Level.prototype.playLevel5 = function () {
+
+}
