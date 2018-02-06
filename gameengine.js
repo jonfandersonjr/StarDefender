b93bafc52c804a52e8335090f34a12bece095772
Tuan Dinh <tuandinhsemail@gmail.com>
2018-02-05 17:19:28 -0800

    minor change

--- gameengine.js
+++ gameengine.js
@@ -79,15 +79,15 @@ GameEngine.prototype.draw = function() {
     if (this.addNewLevel) {
         this.wave.drawWave();
     }
-
-    for (let i = 0; i < this.unitEntities.length; i++) {
-        this.unitEntities[i].draw(this.ctx);
-    }
-
+    
     for (let i = 0; i < this.defenderEntities.length; i++) {
         this.defenderEntities[i].draw(this.ctx);
     }
 
+    for (let i = 0; i < this.unitEntities.length; i++) {
+        this.unitEntities[i].draw(this.ctx);
+    }
+    
     for (let i = 0; i < this.scvEntities.length; i++) {
         this.scvEntities[i].draw(this.ctx);
     }
