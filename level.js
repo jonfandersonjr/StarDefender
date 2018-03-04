0b5e5d3ea983392ab44659a11840242dfd9769a4
jonfanderson <32755821+jonfandersonjr@users.noreply.github.com>
2018-03-04 01:01:18 -0800

    level balancing

--- level.js
+++ level.js
@@ -36,8 +36,8 @@ var firstLevelHealthBuff  = [0, 0, 0, 0, 0, 0, 0, 0, 0]; //unit is buffed by ->>
 var firstLevelEntranceNum = [1, 2, 1, 3, 2, 3, 3, 1, 0] //1 = first entrance, 2 = second entrance, 3 = both entrances
 
 //Level Two (Medium) - Blue
-var secondLevelEnemies     = [dr,z, s, q, h, D, q, g, d, z, d, O, m, d, z, m, g, S];
-var secondLevelWaveSize    = [3, 5, 1, 2, 2, 1, 4, 1, 6, 7, 7, 1, 5, 5, 9, 4, 5, 1, 0];
+var secondLevelEnemies     = [dr,z, s, q, d, D, q, g, d, z, d, O, m, d, z, m, g, S];
+var secondLevelWaveSize    = [3, 5, 1, 2, 2, 1, 4, 1, 6, 7, 7, 1, 3, 5, 9, 4, 5, 1, 0];
 var secondLevelSpeedBuff   = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,.5,.2,.2,.3,.5, 0]; //unit is buffed by ->> unitSpeed * (1 + speedBuff) for readability here
 var secondLevelHealthBuff  = [0, 0, 0, 0, 0,-.2, 0, 0, 0, 1, 0, 0, 0,.5, 1, 0, 1, 0, 0]; //unit is buffed by ->> unitHealth * (1 + healthBuff) for readability here
 var secondLevelEntranceNum = [1, 2, 3, 3, 1, 2, 3, 3, 2, 1, 3, 3, 3, 3, 3, 3, 3, 1, 1]; //1 = first entrance, 2 = second entrance, 3 = both entrances
