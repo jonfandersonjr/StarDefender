c9dac01356bbb2e279cb9f40352ebfb08a3eda16
jonfanderson <32755821+jonfandersonjr@users.noreply.github.com>
2018-03-04 01:29:48 -0800

    balance

--- level.js
+++ level.js
@@ -17,8 +17,8 @@ var h = "hydralisk";      // HP:250  Speed:60  Armor:8
 
 //Flying Enemies
 var s = "scourge";        // HP:40   Speed:65  Armor:0
-var m = "mutalisk";       // HP:110  Speed:50  Armor:6
-var g = "guardian";       // HP:200  Speed:45  Armor:10
+var m = "mutalisk";       // HP:110  Speed:45  Armor:6
+var g = "guardian";       // HP:200  Speed:40  Armor:10
 
 //Special Enemies            //**NOTE: Need one more boss!**//
 var D = "devourer";       // HP:1000  Speed:40  Armor:6     //Increases its own speed once half health
