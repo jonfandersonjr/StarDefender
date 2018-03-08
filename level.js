b4470a35a08c2a5915bf06757f217a622dd302cf
Tuan Dinh <tuandinhsemail@gmail.com>
2018-03-07 22:04:46 -0800

    balancing level 5

--- level.js
+++ level.js
@@ -30,14 +30,14 @@ var S = "sarahkerrigan"; // HP:3000  Speed:40  Armor:10    //Revives upon death 
 
 //var I = "infestedkerrigan";
 
-/*
+
 //Level One (Easy) - Grass
 var firstLevelEnemies     = [i, z, s, l, d, m, q, O, 0];
 var firstLevelWaveSize    = [2, 3, 2, 3, 5, 1, 3, 1, 0];
 var firstLevelSpeedBuff   = [0, 0, 0, 0, 0, 0, 0, 0, 0]; //unit is buffed by ->> unitSpeed * (1 + speedBuff) for readability here
 var firstLevelHealthBuff  = [0, 0, 0, 0, 0, 0, 0, 0, 0]; //unit is buffed by ->> unitHealth * (1 + healthBuff) for readability here
 var firstLevelEntranceNum = [1, 2, 1, 3, 2, 3, 3, 1, 0] //1 = first entrance, 2 = second entrance, 3 = both entrances
-*/
+
 //Level Two (Medium) - Blue
 var secondLevelEnemies     = [dr, z, s, q, h, d, g, O,  z,  d,  q,  z, m,  d,  z,  g,  h,  q, D, 0];
 var secondLevelWaveSize    = [ 3, 5, 1, 2, 2, 3, 1, 1,  5,  7,  5,  8, 3,  5,  9,  2,  5,  4, 1, 0];
@@ -60,11 +60,11 @@ var fourthLevelHealthBuff  = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0,
 var fourthLevelEntranceNum = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 0];
 
 //Level Five (HARD EXTRA) = Ice
-var firstLevelEnemies     = [b, b, i, s, dr, z, l, d, O, z, dr, l, U, m, s, m, q, z, m, O, S, 0];
-var firstLevelWaveSize    = [4, 4, 4, 2,  3, 5, 1, 4, 1, 5,  9, 3, 8, 5, 9, 4, 8, 8, 4, 2, 1, 0];
-var firstLevelSpeedBuff   = [0, 0, 0, 0,  1, 0, 0, 0, 0, 1,  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]; //unit is buffed by ->> unitSpeed * (1 + speedBuff) for readability here
-var firstLevelHealthBuff  = [0, 0, 2, 0,  1, 1, 0, 0, 0, 0,  2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0]; //unit is buffed by ->> unitHealth * (1 + healthBuff) for readability here
-var firstLevelEntranceNum = [1, 2, 2, 1,  3, 1, 2, 2, 1, 3,  1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 0]; //1 = first entrance, 2 = second entrance, 3 = both entrances
+var fifthLevelEnemies     = [b, b, i, s, dr, z, l, d, O, z, dr, l, U, m, l, m, q, z, m, O, S, 0];
+var fifthLevelWaveSize    = [4, 4, 4, 2,  3, 5, 1, 4, 1, 5,  9, 3, 1, 5, 3, 2, 8, 8, 4, 2, 1, 0];
+var fifthLevelSpeedBuff   = [0, 0, 0, 0,  1, 0, 0, 0, 0, 1,  0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0]; //unit is buffed by ->> unitSpeed * (1 + speedBuff) for readability here
+var fifthLevelHealthBuff  = [0, 0, 2, 0,  1, 1, 0, 0, 0, 0,  2, 1, 0, 0, 1, 1, 1, 1, 1, 1, 2, 0]; //unit is buffed by ->> unitHealth * (1 + healthBuff) for readability here
+var fifthLevelEntranceNum = [1, 2, 2, 1,  3, 1, 2, 2, 1, 3,  1, 2, 3, 2, 3, 3, 3, 3, 3, 3, 2, 0]; //1 = first entrance, 2 = second entrance, 3 = both entrances
 
 // test case
 /*
